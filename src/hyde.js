54911993b44fdea237a1efea6a324494e7405003
nuysoft <nuysoft@gmail.com>
2013-12-16 11:12:57 +0800

    重构，删除不必要的函数 Hyde()

--- src/hyde.js
+++ src/hyde.js
@@ -19,61 +19,53 @@
 
 }(function() {
 
-    function Hyde() {}
-
-    Hyde.bind = function bind(data, tpl, callback) {
-        var clone = Loop.watch(data, function(changes) {
-            // console.log(JSON.stringify(changes, null, 2))
-            $.each(changes, function(_, change) {
-                var event = {
-                    target: []
-                }
-                Flush.handle(event, change, clone)
-                if (location.href.indexOf('scrollIntoView') > -1) Flush.scrollIntoView(event, data)
-                Flush.highlight(event, data)
-            })
-        })
-
-        // 定义数据
-        // var clone = Loop.clone(data, true)
-
-        // 修改 AST，插入 Block 占位符
-        var ast = Handlebars.parse(tpl)
-        // console.log(JSON.stringify(ast, null, 4));
-        AST.handle(ast, undefined, undefined, clone.$blocks = {}, clone.$helpers = {})
-        // console.log(JSON.stringify(ast, null, 4));
-
-        // 渲染 HTML
-        var compiled = Handlebars.compile(ast)
-        var html = compiled(clone)
-        // console.log(html)
-
-        // 扫描占位符，定位 Expression 和 Block
-        var content = $('<div>' + html + '</div>')
-        // console.log(content[0].outerHTML)
-        if (content.length) scan(content[0], data)
-        // console.log(content[0].outerHTML)
-        content = content.contents()
-        // console.log(content.length)
-
-
+    return {
         /*
-            TODO 返回什么呢
-            如果传入了 callback()，则返回 data，因为 callback() 的作用在于处理 content；
-            如果 callback() 有返回值，则作为 bind() 的返回值返回，即优先返回 callback() 的返回值；
-            如果未传入 callback，则返回 content，因为不返回 content 的话，content 就会被丢弃。
-
+            Hyde.bind(data, tpl, callback)
         */
-        if (callback) return callback.call(data, content) || data
-        return content
-    }
+        bind: function bind(data, tpl, callback) {
+            var clone = Loop.watch(data, function(changes) {
+                // console.log(JSON.stringify(changes, null, 2))
+                $.each(changes, function(_, change) {
+                    var event = {
+                        target: []
+                    }
+                    Flush.handle(event, change, clone)
+                    if (location.href.indexOf('scrollIntoView') > -1) Flush.scrollIntoView(event, data)
+                    Flush.highlight(event, data)
+                })
+            })
 
-    Hyde.unbind = function unbind(data) {
-        // TODO
-        return
-    }
+            // 定义数据
+            // var clone = Loop.clone(data, true)
 
-    // expose
-    return Hyde
+            // 修改 AST，插入 Block 占位符
+            var ast = Handlebars.parse(tpl)
+            AST.handle(ast, undefined, undefined, clone.$blocks = {}, clone.$helpers = {})
+
+            // 渲染 HTML
+            var compiled = Handlebars.compile(ast)
+            var html = compiled(clone)
+
+            // 扫描占位符，定位 Expression 和 Block
+            var content = $('<div>' + html + '</div>')
+            if (content.length) scan(content[0], data)
+            content = content.contents()
+
+            /*
+                返回什么呢
+                如果传入了 callback()，则返回 data，因为 callback() 的作用在于处理 content；
+                如果 callback() 有返回值，则作为 bind() 的返回值返回，即优先返回 callback() 的返回值；
+                如果未传入 callback，则返回 content，因为不返回 content 的话，content 就会被丢弃。
+
+            */
+            if (callback) return callback.call(data, content) || data
+            return content
+        },
+        unbind: function unbind(data) {
+            // TODO
+            return
+        }
+    }
 
 }));
