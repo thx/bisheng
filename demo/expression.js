f2596d8887264051227a896b3a4ab54025ecb24e
nuysoft <nuysoft@gmail.com>
2013-12-18 16:05:31 +0800

    为示例增加名称

--- demo/expression.js
+++ demo/expression.js
@@ -4,7 +4,7 @@
     var data = Mock.tpl(tpl, {
         title: '@TITLE'
     })
-    doit(data, tpl)
+    doit(data, tpl, arguments.callee.name)
 
     tasks.push(
         function() {
@@ -13,11 +13,11 @@
     )
 })();
 
-(function expression() {
+(function expression_dot() {
     // return
     var tpl = '<span>{{article.title}}</span>'
     var data = Mock.tpl(tpl)
-    doit(data, tpl)
+    doit(data, tpl, arguments.callee.name)
 
     tasks.push(
         function() {
@@ -28,11 +28,11 @@
     )
 })();
 
-(function expression() {
+(function expression_slash() {
     // return
     var tpl = '<span>{{article/title}}</span>'
     var data = Mock.tpl(tpl)
-    doit(data, tpl)
+    doit(data, tpl, arguments.callee.name)
 
     tasks.push(
         function() {
@@ -43,15 +43,30 @@
     )
 })();
 
-(function expression() {
+(function expression_escap() {
     // return
     var tpl = '<span>{{{foo}}}</span>'
     var data = Mock.tpl(tpl)
-    doit(data, tpl)
+    doit(data, tpl, arguments.callee.name)
 
     tasks.push(
         function() {
             data.foo = '<a href="#">' + Random.title(3) + '</a>'
+        }
+    )
+})();
+
+(function expression_placeholder() {
+    // return
+    var tpl = '<span>{{{lines}}}</span>'
+    var data = Mock.tpl(tpl, {
+        lines: '@LINES'
+    })
+    doit(data, tpl, arguments.callee.name)
+
+    tasks.push(
+        function() {
+            data.lines = Random.lines()
         }
     )
 })();
