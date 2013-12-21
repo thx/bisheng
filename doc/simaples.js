a240d5875df80d0293ac99f692cb891b4e9c7020
墨智 <mozhi.gyy@taobao.com>
2013-12-21 20:51:54 +0800

    首页示例

---
+++ doc/simaples.js
@@ -0,0 +1,56 @@
+$(function() {
+
+    var structure = $(
+        Mock.heredoc(function() {
+            /*!
+<div class="tc">
+    <div class="panel panel-default">
+        <div class="panel-heading"></div>
+        <div class="panel-body result"></div>
+    </div>
+</div>
+     */
+        })
+    );
+
+    if (!window.JSON) {
+        window.JSON = {
+            stringify: function(json) {
+                return json
+            }
+        }
+    }
+
+    function doit(data, tpl, name) {
+        var target = structure.clone();
+        Hyde.bind(data, tpl, function(content) {
+            // 可能有多个 'div#simaples'，记录下来，以便当数据变化时更新对应的页面区域
+            target = target
+                .find('div.panel-heading').empty().append(name).end()
+                .find('div.result').empty().append(content).end()
+                .appendTo('div#simaples')
+        })
+    }
+
+    (function attribute_title() {
+        // return
+        var tpl = Mock.heredoc(function() {
+            /*
+            <div class="form-group">
+                <label for="first">First Name:</label>
+                <input class="form-control" id="first" placeholder="Enter first name" value="{{first}}">
+                <label for="last">Last Name:</label>
+                <input class="form-control" id="last" placeholder="Enter last name" value="{{last}}">
+            </div>
+            <p>Hello {{first}} {{last}}!</p>
+            */
+        })
+        var data = {
+            first: '',
+            last: ''
+        }
+        doit(data, tpl, '示例：自动更新 Handlebars 模板')
+    })();
+
+
+})
