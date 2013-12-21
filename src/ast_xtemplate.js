bb896e8d006fc11091fd884f09bc1e9b813fad63
墨智 <mozhi.gyy@taobao.com>
2013-12-21 20:52:15 +0800

    支持 XTemplate（TODO）

---
+++ src/ast_xtemplate.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* global window */
+/* global expose */
+
+(function(factory) {
+
+    expose(factory, function() {
+        window.AST = factory()
+    })
+
+}(function() {
+
+    // TODO 
+    // For KISSY XTemplate 
+
+}));
